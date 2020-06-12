a2064c05fde0ae14be542bbc7920a8f3843a6454
Rafael <rafaelleveske@gmail.com>
2020-06-12 19:05:41 -0300

    Advanced functionalities

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -18,7 +18,14 @@ class PointsController {
       .distinct()
       .select('points.*');
 
-    return res.json(points);
+      const serializedPoints = points.map(point => {
+        return {
+          ...point,
+          image_url: `http://192.168.0.5:3333/tmp/${point.image}`,
+        };
+      });
+  
+      return res.json(serializedPoints);
   }
 
   async show(req: Request, res: Response){
@@ -31,12 +38,18 @@ class PointsController {
       if (!point) {
         return res.status(400).json({ message: "point not found" });
       }
+
+      const serializedPoint = {
+          ...point,
+          image_url: `http://192.168.0.5:3333/tmp/${point.image}`,
+      };
+
       const items = await connection('items')
         .join('point_items', 'items.id', '=', 'point_items.item_id')
         .where('point_items.point_id', id)
         .select('items.title');
 
-      return res.json({ point, items });
+      return res.json({ point: serializedPoint, items });
   }
 
   async create(req: Request, res: Response) {
@@ -54,21 +67,24 @@ class PointsController {
     const trx = await connection.transaction();
 
     const point = {
-      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: req.file.filename,
       name,
       email,
       whatsapp,
       latitude,
       longitude,
       city,
-      uf,
+      uf, 
     };
 
     const insertedIds = await trx('points').insert(point);
 
     const point_id = insertedIds[0];
 
-    const pointItems = items.map((item_id: number) => {
+    const pointItems = items
+    .split(',')
+    .map((item: string) => Number(item.trim()))
+    .map((item_id: number) => {
       return {
         item_id,
         point_id,
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(routes);
 app.use('/tmp', express.static(path.resolve(__dirname, '..', 'tmp')));
 
+app.use(errors());
 
 app.listen(3333);
 
